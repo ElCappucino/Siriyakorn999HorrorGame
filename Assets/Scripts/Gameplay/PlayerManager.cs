c78c1f2bbb5a50766c1fbc635a51e390d4924acc
RodITimMaa <88047496+ElCappucino@users.noreply.github.com>
2025-10-30 22:35:23 +0700

    Reference setup main scene

--- Assets/Scripts/Gameplay/PlayerManager.cs
+++ Assets/Scripts/Gameplay/PlayerManager.cs
@@ -57,18 +57,20 @@ public class PlayerManager : MonoBehaviour
     }
     public void UpdateCurrentTalismanType(string type)
     {
-        switch (type)
+        Debug.Log("type = " + type);
+        string result = type.ToLower();
+        switch (result)
         {
-            case "Lightning":
+            case "lightning":
                 currentTalismanType = TalismanObject.TalismanType.Lighting;
                 break;
-            case "Cross":
+            case "cross":
                 currentTalismanType = TalismanObject.TalismanType.Cross;
                 break;
-            case "Stun":
+            case "stun":
                 currentTalismanType = TalismanObject.TalismanType.Stun;
                 break;
-            case "Thai":
+            case "thai":
                 currentTalismanType = TalismanObject.TalismanType.Thai;
                 break;
             default:
@@ -126,6 +128,10 @@ public class PlayerManager : MonoBehaviour
                 Destroy(proj, 3.0f);
             }
         }
+        else
+        {
+            Debug.Log("GameplayManager.Instance.isGameStart && !cameraControl.isHoldTalisman");
+        }    
         
         
     }
